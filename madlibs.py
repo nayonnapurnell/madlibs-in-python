89ff28bea35c2d74d3ca2e5b822fd17c5a15e482
“Nayonna <Nayonna Purnell>
2020-02-25 23:07:27 -0500

    Created the Madlibs files

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+
+
+
+
+#TODO: Prompt the user for parts of speech and store them in variables
+verb = input("Please enter a verb: ")
+noun = input("Please enter a noun: ")
+adjective = input("Please enter an adjective: ")
+
+#TODO: Output the template to the screen wth the blanks filled out with what the user stated
+print("I enjoy practice!" + "  I find it helps me to "+ verb + " better."
+'\n'"Without practice, my "+ noun + " would probably not even work."
+'\n'"My code is getting more "+ adjective + " every single day!")
